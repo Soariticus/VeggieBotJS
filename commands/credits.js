47c8a4ad0a21ae53dbf4f096df10f3b2a58a651a
lafe <0lafethehorsescrub@gmail.com>
2024-06-04 10:10:11 -0500

    update credits

--- commands/credits.js
+++ commands/credits.js
@@ -14,14 +14,14 @@ module.exports = {
 			.setColor("RANDOM")
 			.setTitle("Credits")
 			.setDescription("Thanks for showing <3")
-			.setFooter("A bot by Soariticus#0666")
+			.setFooter("A bot by Soariticus#0666 and 0lafe")
 			.addFields(
-				{ name: "Developer", value: "Soariticus#0666", inline: true },
-				{ name: "Special thanks to", value: "Bay1k#1799", inline: true},
-				{ name: "Rewritten on", value: "28/9/2021", inline: true},
-				{ name: "Website", value: "https://soaritic.us", inline: true},
+				{ name: "Developer", value: "Soariticus#0666 and 0lafe", inline: true },
+				{ name: "Special thanks to", value: "Bay1k#1799 <3", inline: true},
+				{ name: "Rewritten on", value: "6/3/2024", inline: true},
+				{ name: "Website", value: "https://soaritic.us ", inline: true},
 				{ name: "Times used since last restart", value: used.toString(), inline: true},
-				{ name: "Want VeggieBot in your server?", value: "Message Soariticus#0666", inline:true },
+				{ name: "Want VeggieBot in your server?", value: "Message 0lafe", inline:true },
 			);
 		
 		await interaction.reply({embeds: [buildEmbed]});
